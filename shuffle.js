f3a76ce43841a81ca8ea2c7681ffd83f66243b0d
0edk <232494884+0edk@users.noreply.github.com>
2026-05-11 23:15:43 -0400

    Cite sources in shuffle script

--- shuffle.js
+++ shuffle.js
@@ -4,6 +4,7 @@ function shuffle() {
     for (s of children) {
         s.remove();
     }
+    // https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle#JavaScript_implementation
     for (let i = children.length - 1; i >= 1; i--) {
         const j = Math.floor(Math.random() * (i + 1));
         [children[i], children[j]] = [children[j], children[i]];
@@ -12,6 +13,7 @@ function shuffle() {
         container.append(s);
     }
 }
+// https://old.reddit.com/r/Anki/comments/103b97l/help_javascript_template_code_only_works_on_first/j2zsp36/
 new Promise(resolve => {
     if (document.readyState == "loading") {
         document.addEventListener("DOMContentLoaded", resolve);
